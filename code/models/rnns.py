2407239c2c2baafc2f9a1174ff9182037bda7b1d
charlesdgburns <charlesdgburns@gmail.com>
2025-08-25 23:56:05 +0000

    Trainer class with basic triain,val,eval - also added trail_by_trial outputs

--- code/models/rnns.py
+++ code/models/rnns.py
@@ -16,15 +16,49 @@ import numpy as np
 
 ## Top level agent ## 
 
+
 class TinyRNN(nn.Module):
-    pass
+  def __init__(self, input_size:int=3, hidden_size:int=1, out_size:int=2,
+               rnn_type = 'GRU',
+               sparsity_lambda:float = 1e-2):
+    super().__init__()
+    self.I = input_size
+    self.H = hidden_size
+    self.O = out_size
+    self.rnn_type = rnn_type
+    if rnn_type == 'GRU':
+      self.rnn = ManualGRU(self.I,self.H)
+    elif rnn_type == 'LSTM':
+      self.rnn = ManualLSTM(self.I,self.H)
+    #elif model_type == 'NMRNN':
+    #  self.rnn = ManualNMRNN(self.I,self.H)
+    self.decoder = nn.Linear(self.H, self.O)
+    self.L1 = sparsity_lambda
+
+  def forward(self, inputs):
+    hidden, _ = self.rnn(inputs)
+    predictions = self.decoder(hidden)
+    return predictions
+
+  def compute_losses(self, predictions, targets):
+    prediction_loss = nn.functional.cross_entropy(predictions, targets) #NB: this applies softmax itself
+    #for sparsity we need to select the right weights to regularise
+    sparsity_loss = 0
+    for name, param in self.rnn.named_parameters():
+      if 'bias' not in name:
+        sparsity_loss += self.L1*torch.abs(param).sum()
+    return prediction_loss, sparsity_loss
+
 
 
 ## Manual RNN architectures ##
 
 
+
 class ManualGRU(nn.Module):
-  '''Manual GRU coded with gate_activations() to return gate activations dictionary'''
+  '''Manual GRU coded to return gate activations dictionary
+  Returns: (hidden_sequence, h_past) if return_gate_activations=False,
+           (hidden_sequence, gate_activations) if -||- = True'''
   def __init__(self,input_size,hidden_size):
     super().__init__() #init nn.Module
     self.input_size = input_size
@@ -42,7 +76,8 @@ class ManualGRU(nn.Module):
     for weight in self.parameters():
         weight.data.uniform_(-stdv, stdv)
 
-  def forward(self, inputs, init_states = None, return_gate_activations=False):
+  def forward(self, inputs, init_states = None, 
+              return_gate_activations=False):
     ''' inputs are a tensor of shape (batch_size, sequence_size, input_size)
         outputs are tensor of shape (batch_size, sequence_size, hidden_size)'''
 
@@ -65,21 +100,21 @@ class ManualGRU(nn.Module):
       r_t = self.sigmoid(from_input[:,0]+from_hidden[:,0]) #(n_batch,n_hidden), ranging from 0 to 1
       z_t = self.sigmoid(from_input[:,1]+from_hidden[:,1]) #(n_batch,n_hidden), ranging from 0 to 1; must have n_hidden because it is multiplied with hidden_state later.
       if return_gate_activations:
-        gate_activations['reset'].append(r_t) 
-        gate_activations['update'].append(z_t)
+        gate_activations['reset'].append(r_t.unsqueeze(0)) 
+        gate_activations['update'].append(z_t.unsqueeze(0))
       n_t = self.tanh(from_input[:,2]+r_t*(from_hidden[:,2])).view(batch_size, self.hidden_size) #(n_batch,n_hidden)
       h_past = (1-z_t)*n_t + z_t*h_past #(n_batch,hidden_size) #NOTE h_past is tehnically h_t now, but in the next for-loop it will be h_past. ;)
       hidden_sequence.append(h_past.unsqueeze(0)) #appending (1,n_batch,n_hidden) to a big list.
 
+    hidden_sequence = torch.cat(hidden_sequence, dim=0) #(n_sequence, n_batch, n_hidden) gather all inputs along the first dimenstion
+    hidden_sequence = hidden_sequence.transpose(0, 1).contiguous() #reshape to batch first (n_batch,n_seq,n_hidden)
+    
     if return_gate_activations:
         for gate_label, activations in gate_activations.items():
             gate_activations[gate_label] = torch.cat(activations, dim=0).transpose(0,1).contiguous() #(n_batch,n_seq,n_hidden)
-        return gate_activations
-
-    hidden_sequence = torch.cat(hidden_sequence, dim=0) #(n_sequence, n_batch, n_hidden) gather all inputs along the first dimenstion
-    hidden_sequence = hidden_sequence.transpose(0, 1).contiguous() #reshape to batch first (n_batch,n_seq,n_hidden)
-    return hidden_sequence, h_past #this is standard in Pytorch, to output sequence of hidden states alongside most recent hidden state.
-
+        return hidden_sequence, gate_activations
+    else:
+      return hidden_sequence, h_past #this is standard in Pytorch, to output sequence of hidden states alongside most recent hidden state.
 
 
 class ManualLSTM(nn.Module):
@@ -88,8 +123,8 @@ class ManualLSTM(nn.Module):
         self.device=device
         self.input_sz = input_sz
         self.hidden_size = hidden_sz
-        self.W_from_in = nn.Parameter(torch.Tensor(input_sz, hidden_sz * 4))
-        self.W_from_h = nn.Parameter(torch.Tensor(hidden_sz, hidden_sz * 4))
+        self.W = nn.Parameter(torch.Tensor(input_sz, hidden_sz * 4))
+        self.U = nn.Parameter(torch.Tensor(hidden_sz, hidden_sz * 4))
         self.bias = nn.Parameter(torch.Tensor(hidden_sz * 4))
         self.init_weights()
 
@@ -116,7 +151,7 @@ class ManualLSTM(nn.Module):
         for t in range(seq_sz):
             x_t = x[:, t, :]
             # batch the computations into a single matrix multiplication
-            gates = x_t @ self.W_from_in + h_t @ self.W_from_h + self.bias
+            gates = x_t @ self.W + h_t @ self.U + self.bias
             i_t, f_t, g_t, o_t = (
                 torch.sigmoid(gates[:, :HS]), # input
                 torch.sigmoid(gates[:, HS:HS*2]), # forget
@@ -137,3 +172,4 @@ class ManualLSTM(nn.Module):
             return hidden_sequence, gate_activations
         else:
             return hidden_sequence, (h_t, c_t)
+
